55393d30b3424da74152c91e059492f3d9031a12
Houssem-Ellouze <houssem.ellouze@esprit.tn>
2025-02-18 10:29:01 +0100

    "Ajout de .idea au .gitignore

---
+++ Goal_Reward/src/main/java/tn/esprit/goal_reward/Repository/RewardRepository.java
@@ -0,0 +1,6 @@
+package tn.esprit.goal_reward.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import tn.esprit.goal_reward.Entity.Reward;
+
+public interface RewardRepository extends MongoRepository<Reward, Integer> {
+}
