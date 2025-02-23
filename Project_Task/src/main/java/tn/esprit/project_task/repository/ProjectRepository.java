266eccfb4441517082e8ae092644efa60ff18ab9
mohamedwassimbahria <mohamedwassim.bahrialasgher@esprit.tn>
2025-02-23 19:44:32 +0000

    add function repository

--- Project_Task/src/main/java/tn/esprit/project_task/repository/ProjectRepository.java
+++ Project_Task/src/main/java/tn/esprit/project_task/repository/ProjectRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.project_task.entity.Project;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends MongoRepository<Project, String> {
+
+    List<Project> findByTitle(String title);
 }
