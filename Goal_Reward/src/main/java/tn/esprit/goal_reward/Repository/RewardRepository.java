0b52927d8a27bdfa815ff758ddc4435f2e2dbf3a
mahdi-mzoughi <mahdibenammor@gmail.com>
2025-02-25 18:40:30 +0100

    update

--- Goal_Reward/src/main/java/tn/esprit/goal_reward/Repository/RewardRepository.java
+++ Goal_Reward/src/main/java/tn/esprit/goal_reward/Repository/RewardRepository.java
@@ -2,5 +2,5 @@ package tn.esprit.goal_reward.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import tn.esprit.goal_reward.Entity.Reward;
 
-public interface RewardRepository extends MongoRepository<Reward, Integer> {
+public interface RewardRepository extends MongoRepository<Reward, String> {
 }
